2a433d0bde1276344d59cf1b3ec251f8fac84fef
Kashyap Nathan <kashyapnathan2@gmail.com>
2023-10-14 14:13:21 -0400

    adding brqanch and chakra

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+import joblib
+import numpy as np
 import random
 
 app = Flask(__name__)
-CORS(app)  # enabling CORS
+CORS(app)  
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Load the model (replace with your model's path)
+model = joblib.load('boosting.joblib')
 
 # Sample Data
 recent_frauds = [{'transaction_id': 'tx001'}, {'transaction_id': 'tx002'}]
@@ -33,27 +38,30 @@ def get_user_transactions(user_id):
 @app.route('/api/fraud-check', methods=['POST'])
 def fraud_check():
     transaction_data = request.json.get('transactionData')
-    dummy_response = {'is_fraudulent': False}
-    return jsonify(dummy_response)
+    
+    # Ensure to process the input data as per your model’s requirements
+    features = np.array([transaction_data[key] for key in transaction_data]).reshape(1, -1)
+    prediction = model.predict(features)
+    
+    is_fraudulent = prediction[0] == 1  
+    return jsonify({'is_fraudulent': is_fraudulent})
 
 @app.route('/api/submit-transaction', methods=['POST'])
 def submit_transaction():
     transaction_data = request.json.get('transactionData')
     
     # Logic to process and validate the transaction data
-    # For example, check for fraud and save the transaction to the database
+    # Check for fraud and save the transaction to the database
+    # ...
 
-    # Example: Update recent frauds and notify clients
     if random.choice([True, False]):  # Simulate a fraud detection
         recent_frauds.append(transaction_data)
         socketio.emit('new_fraud', transaction_data)
     
-    # Example: Update user transactions and notify clients
     user_transactions.append(transaction_data)
     socketio.emit('new_user_transaction', transaction_data)
     
-    # Example: Update fraud graph data and notify clients
-    new_fraud_count = random.randint(0, 10)  # Simulate fraud count data
+    new_fraud_count = random.randint(0, 10)  
     graph_data.append({'time': '2022-01-02', 'fraud_count': new_fraud_count})
     socketio.emit('update_graph', graph_data[-1])
     

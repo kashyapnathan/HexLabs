77399c10eaed2bbc91fec200e688d15f8a951ecb
ARYAN_GARG <agarg396@gatech.edu>
2023-10-15 02:10:33 -0400

    anything

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
 import './App.css';
-import TransactionChecker from './components/TransactionChecker';
 import DetectionScoreViewer from './components/DetectionScoreViewer';
-import FraudGraph from './components/FraudGraph';
-import FraudAnaltyics from './components/FraudAnaltyics';
-import UserTransactions from './components/UserTransactions';
 import Slider from './components/Slider';
 
 function App() {
   const [threshold, setThreshold] = useState(50);
+  const [transactionId, setTransactionId] = useState('');
+  const [score, setScore] = useState(null);
 
   const handleSliderChange = (event) => {
     setThreshold(Number(event.target.value));
+  };
+
+  const handleTransactionIdChange = (event) => {
+    setTransactionId(event.target.value);
+  };
+
+  const handleDetectFraud = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/api/get-detection-score/${transactionId}`);
+      const data = await response.json();
+
+      if (response.ok) {
+        setScore(data.score);
+      } else {
+        console.error('Error fetching detection score:', data.error);
+        setScore(null);
+      }
+    } catch (error) {
+      console.error('Error fetching detection score:', error);
+      setScore(null);
+    }
   };
 
   return (
     <div className="App">
       <h1 className="title"> Fraud Dashboard </h1>
       <Slider threshold={threshold} onSliderChange={handleSliderChange} />
-      <DetectionScoreViewer transactionId="sample_transaction_id" threshold={threshold} />
-      <div className="history">
-        <FraudAnaltyics />
-      </div>
-      <UserTransactions userId="sample_user_id" />
+      <input
+        type="text"
+        placeholder="Enter Transaction ID"
+        value={transactionId}
+        onChange={handleTransactionIdChange}
+      />
+      <button onClick={handleDetectFraud}>Detect Fraud</button>
+      <DetectionScoreViewer transactionId={transactionId} threshold={threshold} score={score} />
     </div>
   );
 }
--- my-app/src/components/DetectionScoreViewer.js
+++ my-app/src/components/DetectionScoreViewer.js
@@ -2,50 +2,69 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DetectionScoreViewer({ transactionId, threshold }) {
-  const [score, setScore] = useState(70);
-  const [transID, setID] = useState("XXXXXXXXXXX");
-  const [name, setName] = useState("Jeffrey Dahmer");
-  const [circleColor, setCircleColor] = useState('gray'); // Initialize with gray
+  const [score, setScore] = useState(70);
+  const [transID, setID] = useState("XXXXXXXXXXX");
+  const [name, setName] = useState("Jeffrey Dahmer");
+  const [circleColor, setCircleColor] = useState('gray'); // Initialize with gray
 
   useEffect(() => {
-    const fetchDetectionScore = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/api/get-detection-score/${transactionId}`);
-        setScore(response.data.score);
-      } catch (error) {
-        console.error('Error fetching detection score:', error);
-      }
-    };
-    fetchDetectionScore();
-  }, [transactionId]);
+    const fetchDetectionScore = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/get-detection-score/${transID}`);
+        setScore(response.data.score);
+      } catch (error) {
+        console.error('Error fetching detection score:', error);
+      }
+    };
+    fetchDetectionScore();
+  }, [transID]);
 
-  // Calculate the circle color based on the threshold and score
-  useEffect(() => {
-    if (score !== null) {
-      setCircleColor(score >= threshold ? '#913831' : 'green');
-    }
-  }, [score, threshold]);
+  
 
-  return (
-    <div className="userInfo">
-      <div className="circle" style={{ backgroundColor: circleColor }}>
-        {score !== null ? (
-          <p className="score">{score}</p>
-        ) : (
-          <p className="loading">Loading score...</p>
-        )}
-      </div>
-      <h2 className="fraudScoreText"> Fraud Score </h2>
-      <div className="infoPart">
-        <p> Transaction ID: {transID} </p>
-        <p> Name: {name} </p>
-        {/* Add the fetch score button */}
-        <button className="Next">
-          Next
-        </button>
-      </div>
-    </div>
-  );
+
+  // Calculate the circle color based on the threshold and score
+  useEffect(() => {
+    if (score !== null) {
+      setCircleColor(score >= threshold ? '#913831' : 'green');
+    }
+  }, [score, threshold]);
+
+
+
+  return (
+    <div className="userInfo">
+      <div className = "userPart">
+      <div className="circle" style={{ backgroundColor: circleColor }}>
+        {score !== null ? (
+          <p className="score">{score}</p>
+        ) : (
+          <p className="loading">Loading score...</p>
+        )}
+      </div>
+      <h2 className="fraudScoreText"> Fraud Score </h2>
+      </div>
+      <div className="infoPart">
+        <p> Transaction ID: {transID} </p>
+        <p> Name: {name} </p>
+        {score > threshold ? (
+          <div class = "alert">
+            {/* HTML to insert if the score is greater than the threshold */}
+            <p>Your score is greater than the threshold!</p>
+            <p>Check the customers ID</p>
+          </div>
+        ) : 
+        (<div> 
+        <br></br>
+        <br></br>
+        <br></br>
+        </div>) }
+        <button className="Next">
+          Next
+        </button>
+      </div>
+      </div>
+      
+  );
 }
 
 export default DetectionScoreViewer;
